12cdfa7f1e585d0d02fe72a122b575afcb0781e3
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-24 18:42:58 -0400

    update

--- EA2/PaginajQueryValidar.js
+++ EA2/PaginajQueryValidar.js
@@ -1,14 +1,46 @@
 $(function()
 {
-    // TODOS EL CODIGO JQUERY
-    $('.divOcultar').hide(); // oculta el div
+    let numero = '1234567890';
+    let letras = 'qwertyuiopasdfghjklñzxcvbnmQWERTYUIOPASDFGHJKLÑZXCVBNMáéíóúÁÉÍÓÚ';
+// TAREA: validar dv, nombre y correo
+    $('.txtRut').keypress(function(e)
+    {
+        let caracter = String.fromCharCode(e.which);
+        if(numero.indexOf(caracter) < 0)
+            return false;
+
+    })
 
     $('.btnAceptar').click(function()
     {
-        let nombre = $('.txtNombre').val(); // obtener el valor del input
-        //alert("Hola " + nombre + ", ¿Cómo estás?");
+        if(!$('.txtRut').val())
+        {
+            alert("Falta el rut");
+            $('.txtRut').focus();
+        }
+        else if(!$('.txtDv').val())
+        {
+            alert("Falta el dv");
+            $('.txtDv').focus();
+        }
+        else if(!$('.txtNombre').val())
+        {
+            alert("Falta el nombre");
+            $('.txtNombre').focus();
+        }
+        else if(!$('.txtEmail').val())
+        {
+            alert("Falta el email");
+            $('.txtEmail').focus();
+        }
+    })
+    $('.btnLimpiar').click(function()
+    {
+        $('.txtRut, .txtDv, .txtNombre, .txtEmail').val('');
+        /*$('.txtRut').val('');
+        $('.txtDv').val('');
         $('.txtNombre').val('');
-        $('.txtResultado').val(nombre);
-        $('.divOcultar').show(); // muestra el div
+        $('.txtEmail').val('');*/
+        $('.txtRut').focus();
     })
 })
