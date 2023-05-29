e34deba00bdbf422823b236961d8efd4fe8a569a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-29 17:31:00 -0400

    update

--- EA3/proyecto003/instituto/crud/admin.py
+++ EA3/proyecto003/instituto/crud/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Marca
+from .models import Marca, Categoria
 # Register your models here.
 admin.site.register(Marca)
+admin.site.register(Categoria)
--- EA3/proyecto003/instituto/crud/models.py
+++ EA3/proyecto003/instituto/crud/models.py
@@ -9,6 +9,44 @@ class Marca(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+
+class Categoria(models.Model):
+    idCategoria = models.AutoField(db_column='idCategoria', primary_key=True)
+    nombre = models.CharField(db_column='nombre', max_length=50, null=False, unique=True)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+    
+
+class Genero(models.Model):
+    idGenero = models.AutoField(db_column='idGenero', primary_key=True)
+    nombre = models.CharField(db_column='nombre', max_length=50, null=False, unique=True)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+    
+
+class Cliente(models.Model):
+    idCliente = models.AutoField(db_column='idCliente', primary_key=True)
+    rut = models.IntegerField(unique=True, null=False, blank=False)
+    dv = models.CharField(max_length=1, null=False, blank=False)
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    apellido = models.CharField(max_length=50, null=False, blank=False)
+    email = models.CharField(max_length=50, null=False, blank=False)
+    telefono = models.CharField(max_length=50, null=False, blank=False)
+    genero = models.ForeignKey(Genero, db_column='idGenero', 
+                                        on_delete=models.CASCADE, null=False, blank=False)
+    fechaNacimiento = models.DateField(null=False, blank=False)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+    
+
+
 # PASOS:
 # 1.- para preparar la migración:
 # py manage.py makemigrations crud
